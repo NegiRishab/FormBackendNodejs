af1e3ca32c5fb3cdb174d0a34962a0329c8797a1
NegiRishab <rishabhvicky98@gmail.com>
2024-01-26 16:06:04 +0530

    chnges

--- api/index.js
+++ api/index.js
@@ -50,7 +50,7 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 
 require('dotenv').config();
-const formroute = require('./formdata/route')
+const formroute = require('../formdata/route')
 const app = express();
 const port = 3001;
 
